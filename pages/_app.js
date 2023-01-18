759bdaa2c140644b5ae4fd26ef77f252414f1cb4
Santos <webdeveloper510>
2023-01-18 11:14:14 +0530

    add some files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { SessionProvider } from "next-auth/react"
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/globals.css";
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({

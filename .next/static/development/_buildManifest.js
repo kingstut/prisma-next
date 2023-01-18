3f5b994feaf8b6c9f9d30b7ea2775481f0e87290
Santos <webdeveloper510>
2023-01-18 14:50:20 +0530

    del ./next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/first":["static\u002Fchunks\u002Fpages\u002Ffirst.js"],"/second":["static\u002Fchunks\u002Fpages\u002Fsecond.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ffirst","\u002Fsecond"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
